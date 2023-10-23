8533b3f245e3719dfc84e1437c3b73179619460e
micheal-p <58996608+micheal-p@users.noreply.github.com>
2023-10-23 13:45:21 +0100

    landingpage.js

--- doctor/landingpage.js
+++ doctor/landingpage.js
@@ -1,18 +1,47 @@
 const textElement = document.getElementById('typing-text');
 const text = "We’ve developed and trained a knowledge-based model called Assistant Doctor which interacts in a conversational way. The dialogue format makes it possible for Assistant Doctor to answer followup questions, admit its mistakes, challenge incorrect premises, and reject inappropriate requests.";
-let charIndex = 0;
 
-function typeText() {
-    if (charIndex < text.length) {
-        textElement.textContent += text.charAt(charIndex);
-        charIndex++;
+function printText() {
+    textElement.textContent = text; 
+
+
+if (window.innerWidth <= 480) {
+   
+    printText();
+} else if (window.innerWidth <= 768) {
+   
+    let charIndex = 0;
+
+    function typeText() {
+        if (charIndex < text.length) {
+            textElement.textContent += text.charAt(charIndex);
+            charIndex++;
+        }
     }
-}
 
-// Call the typeText function at a set interval (e.g., every 50 milliseconds)
-const typingInterval = setInterval(typeText, 50);
+    // Call the typeText function at a set interval (e.g., every 50 milliseconds)
+    const typingInterval = setInterval(typeText, 50);
 
-// Stop the interval when the entire text has been typed
-if (charIndex === text.length) {
-    clearInterval(typingInterval);
+    // Stop the interval when the entire text has been typed
+    if (charIndex === text.length) {
+        clearInterval(typingInterval);
+    }
+} else {
+    // Use typing animation for larger screens (e.g., desktop)
+    let charIndex = 0;
+
+    function typeText() {
+        if (charIndex < text.length) {
+            textElement.textContent += text.charAt(charIndex);
+            charIndex++;
+        }
+    }
+
+    // Call the typeText function at a set interval (e.g., every 50 milliseconds)
+    const typingInterval = setInterval(typeText, 50);
+
+    // Stop the interval when the entire text has been typed
+    if (charIndex === text.length) {
+        clearInterval(typingInterval);
+    }
 }
